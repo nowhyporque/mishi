f4a78c8127aedf871ba6fa6377f26a97b3a19e45
Kevin Villa <sebastian1722@outlook.com>
2026-02-09 18:49:35 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkZowfWWfKEtzHjfMDwocwj4nAXLMSGgk",
-  authDomain: "mishi-db445.firebaseapp.com",
-  projectId: "mishi-db445",
-  storageBucket: "mishi-db445.firebasestorage.app",
-  messagingSenderId: "401657833268",
-  appId: "1:401657833268:web:c689bf42f6ae47e386e761",
-  measurementId: "G-WDVDZD1BQZ"
+  
 };
 
 
